0f2991027762029e566a1311ec02377d32962e1b
AayushiGandhi <ay753882@dal.ca>
2022-04-21 18:51:23 -0300

    Initial commit

---
+++ src/array/MaxMin.java
@@ -0,0 +1,7 @@
+package array;
+
+public class MaxMin {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/array/ReverseArray.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static int[] reverseArray(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString( reverseArray(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(Arrays.toString( reverseArray(new int[]{1, 2, 3, 4, 5, 6})));
+    }
+}
+
